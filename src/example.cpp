32acd653a5e7fbf14dcd926ee5c36f1f666943ca
Tommi Mäklin <tommi@maklin.fi>
2019-10-07 14:51:16 +0300

    Update the example.

--- src/example.cpp
+++ src/example.cpp
@@ -10,15 +10,20 @@ int main(int argc, char** argv) {
   args.add_argument<std::string>("s", "string", "This is a string.");
   args.add_argument<std::vector<int>>("l", "list", "This is a list of integers.");
   args.parse(argc, argv);
+
   double val = args.value<double>("double");
-  std::cout << "Value of the double: " << val << std::endl;
+  std::cout << "Value of the double (argument nr. " << args.get_pos("double") << "): " << val << std::endl;
+
   bool gzip = args.value<bool>("gzip");
-  std:: cout << "Toggle is: " << (gzip ? "true" : "false") << std::endl;
+  std::cout << "Toggle --gzip (argument nr. " << args.get_pos("gzip") << "): " << (gzip ? "true" : "false") << std::endl;
+
   std::string str = args.value<std::string>("string");
-  std:: cout << "String reads: " << str << std::endl;
+  std::cout << "Text in the string (argument nr. " << args.get_pos("string") << "): " << str << std::endl;
+
   std::vector<int> ints = args.value<std::vector<int>>("list");
+  std::cout << "List of integers (argument nr. " << args.get_pos("list") << "):" << std::endl;
   for (size_t i = 0; i < ints.size(); ++i) {
-    std::cout << "Integer in the list at pos " << i << " is: " << ints.at(i) << std::endl;
+    std::cout << '\t' << ints.at(i) << " at position " << i << std::endl;
   }
 
   return 0;
