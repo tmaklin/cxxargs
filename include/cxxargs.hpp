278af5139149f257faaf8ceac120c230a229a83c
Tommi Mäklin <tommi@maklin.fi>
2019-10-07 12:02:08 +0300

    Const some functions and parameters that shouldn't modify anything.

--- include/cxxargs.hpp
+++ include/cxxargs.hpp
@@ -28,7 +28,7 @@ namespace cxxargs {
   class Argument {
   public:
     virtual uint16_t get_pos() const =0;
-    virtual std::string get_help() =0;
+    virtual std::string get_help() const =0;
     virtual void parse_arg(char** begin, char** end) =0;
     template <class T> const T& get_val() const;
   };
@@ -41,7 +41,8 @@ namespace cxxargs {
     std::string help_text;
     T val;
     uint16_t pos;
-    std::pair<char**, uint16_t> FindArg(char **begin, char **end) {
+
+    std::pair<char**, uint16_t> FindArg(char **begin, char **end) const {
       char **it = std::find(begin, end, this->short_name);
       if (it == end) {
 	it = std::find(begin, end, this->long_name);
@@ -49,6 +50,7 @@ namespace cxxargs {
       uint16_t pos = std::ceil((std::distance(begin, it) + 1)/2.0);
       return std::make_pair(it, pos);
     }
+
   public:
     ArgumentVal();
     ArgumentVal(std::string a, std::string b, std::string c) {
@@ -68,22 +70,25 @@ namespace cxxargs {
       }
       this->pos = at.second;
     }
-    std::string get_help() override { return this->help_text; }
+    std::string get_help() const override { return this->help_text; }
     const T& get_val() const { return this->val; };
     uint16_t get_pos() const override { return this->pos; }
   };
-  template<class T> const T& Argument::get_val() const {
-    return dynamic_cast<const ArgumentVal<T>&>(*this).get_val();
-  }
+
   template<> void ArgumentVal<bool>::parse_arg(char** begin, char** end) {
     std::pair<char**, uint16_t> at = FindArg(begin, end);
     this->pos = at.second;
     this->val = ((at.first != end) ^ this->val);
   }
 
+  template<class T> const T& Argument::get_val() const {
+    return dynamic_cast<const ArgumentVal<T>&>(*this).get_val();
+  }
+
   class Arguments {
   private:
     std::map<std::string, std::shared_ptr<Argument>> args;
+
   public:
     template <typename T> void add_argument(std::string short_name, std::string long_name, std::string help_text) {
       this->args.insert(std::make_pair(long_name, std::shared_ptr<Argument>(new ArgumentVal<T>(short_name, long_name, help_text))));
@@ -91,12 +96,12 @@ namespace cxxargs {
     template <typename T> void add_argument(std::string short_name, std::string long_name, std::string help_text, T in_val) {
       this->args.insert(std::make_pair(long_name, std::shared_ptr<Argument>(new ArgumentVal<T>(short_name, long_name, help_text, in_val))));
     }
-    void parse(int argc, char** argv) {
+    void parse(int argc, char** argv) const {
       for (auto kv : args) {
 	kv.second->parse_arg(argv, argv+argc);
       }
     }
-    std::string help() {
+    std::string help() const {
       std::string help_text("");
       for (auto kv : args) {
 	help_text += kv.second->get_help();
@@ -104,8 +109,8 @@ namespace cxxargs {
       }
       return help_text;
     }
-    template <typename T> T value(const std::string &name) { return args.at(name)->get_val<T>(); }
-    uint16_t get_pos(const std::string &name) { return args.at(name)->get_pos(); }
+    template <typename T> T value(const std::string &name) const { return args.at(name)->get_val<T>(); }
+    uint16_t get_pos(const std::string &name) const { return args.at(name)->get_pos(); }
   };
 }
 
