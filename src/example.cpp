dca9cd050cb4ab84d7f619a4f1fe34241959daf6
Tommi Mäklin <tommi@maklin.fi>
2019-10-07 14:58:00 +0300

    Add license.

--- src/example.cpp
+++ src/example.cpp
@@ -1,7 +1,10 @@
-#include "cxxargs.hpp"
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 #include <iostream>
 
-#include "cxxargs_defined.hpp"
+#include "cxxargs.hpp"
 
 int main(int argc, char** argv) {
   cxxargs::Arguments args;
