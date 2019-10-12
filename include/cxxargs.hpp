4fef853ff4e544dec6a674399cb0af9e37a656d2
Tommi Mäklin <tommi@maklin.fi>
2019-10-12 14:54:59 +0300

    Move declarations around & consistensify style.

--- include/cxxargs.hpp
+++ include/cxxargs.hpp
@@ -42,79 +42,78 @@ namespace cxxargs {
   }
 
   class Argument {
-  private:
+   private:
     char short_name;
     std::string long_name;
     std::string help_text;
 
-  public:
+   public:
     Argument();
     Argument(char short_name, std::string long_name, std::string help_text)
       : short_name(short_name)
       , long_name("--" + long_name)
       , help_text("-" + std::string(1, this->short_name) + " " + this->long_name + "\t" + help_text) {};
 
-    virtual void parse_argument(std::vector<std::string>::const_iterator iter) =0;
     virtual void parse_argument(std::stringstream &str) =0;
-    virtual const bool& is_initialized() const =0;
-    template <class T> const T& get_val() const;
+    virtual void parse_argument(std::vector<std::string>::const_iterator iter) =0;
     template <class T, class U> void set_val(U& in_val);
 
+    virtual const bool& is_initialized() const =0;
+    template <class T> const T& get_val() const;
     const std::string& get_help() const { return this->help_text; }
   };
 
   template <typename T>
   class ArgumentVal : public Argument {
-  private:
+   private:
     T val;
     bool value_initialized = false;
 
-  public:
+   public:
     using Argument::Argument;
     ArgumentVal(char short_name, std::string long_name, std::string help_text, T in_val)
       : Argument(short_name, long_name, help_text) {
       this->set_val(in_val);
     }
-    const T& get_val() const { return this->val; };
-    void set_val(T& in_val) { this->value_initialized = true; this->val = in_val; }
-    const bool& is_initialized() const override { return this->value_initialized; }
 
     void parse_argument(std::stringstream &str) override {
       T in_val;
       str >> in_val;
       this->set_val(in_val);
     }
-
     void parse_argument(std::vector<std::string>::const_iterator iter) override {
       ++iter;
       std::stringstream str(*iter);
       parse_argument(str);
     }
-  };
+    void set_val(T& in_val) { this->value_initialized = true; this->val = in_val; }
 
+    const bool& is_initialized() const override { return this->value_initialized; }
+    const T& get_val() const { return this->val; };
+  };
   template<> void ArgumentVal<bool>::parse_argument(std::vector<std::string>::const_iterator iter) {
     bool in_val = (this->is_initialized() ? !this->get_val() : true);
     this->set_val(in_val);
   }
-  
-  template<class T> const T& Argument::get_val() const {
-    return dynamic_cast<const ArgumentVal<T>&>(*this).get_val();
-  }
   template<class T, class U> void Argument::set_val(U& in_val) {
     return dynamic_cast<ArgumentVal<T>&>(*this).set_val(in_val);
   }
+  template<class T> const T& Argument::get_val() const {
+    return dynamic_cast<const ArgumentVal<T>&>(*this).get_val();
+  }
 
   class Arguments {
-  private:
+   private:
     std::map<std::string, std::shared_ptr<Argument>> args;
     std::map<char, std::shared_ptr<Argument>> shortargs;
     std::vector<std::string> positionals;
     std::string help_text;
     std::string program_name;
 
-  public:
+   public:
     Arguments(std::string program_name, std::string usage_info)
       : program_name(program_name), help_text(usage_info) {};
+
     template <typename T> void add_argument(char short_name, std::string long_name, std::string help_text) {
       this->args.insert(std::make_pair("--" + long_name, std::shared_ptr<Argument>(new ArgumentVal<T>(short_name, long_name, help_text))));
       this->shortargs.insert(std::make_pair(short_name, this->args.at("--" + long_name)));
@@ -150,7 +149,8 @@ namespace cxxargs {
       }
     }
 
-    const std::string& help() const { return this->help_text; };
+    size_t n_positionals() const { return this->positionals.size(); }
+
     template <typename T> const T& value(const std::string &name) const {
       if (this->args.find("--" + name) == this->args.end()) {
 	throw exceptions::argument_not_found(name);
@@ -159,9 +159,9 @@ namespace cxxargs {
       }
       return this->args.at("--" + name)->get_val<T>();
     }
+    const std::string& help() const { return this->help_text; };
     const std::string& get_program_name() const { return this->program_name; }
     const std::string& get_positional(const uint16_t &pos) const { return this->positionals.at(pos); }
-    size_t n_positionals() const { return this->positionals.size(); }
   };
 }
 
