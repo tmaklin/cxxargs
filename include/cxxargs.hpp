01d0d5fe12473af6f0e396eb109e9193ed9c892c
Tommi Mäklin <tommi@maklin.fi>
2019-10-31 15:45:20 +0200

    Bump minor version.

--- include/cxxargs.hpp
+++ include/cxxargs.hpp
@@ -13,8 +13,8 @@
 #include <exception>
 
 #define CXXARGS_VERSION_MAJOR 1
-#define CXXARGS_VERSION_MINOR 0
-#define CXXARGS_VERSION_PATCH 1
+#define CXXARGS_VERSION_MINOR 1
+#define CXXARGS_VERSION_PATCH 0
 
 namespace cxxargs {
   namespace exceptions {
